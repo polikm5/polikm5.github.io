32ac9abaec194a6302509310bb7dbb6e51fc8ba3
polikm5 <864423284@qq.com>
2020-12-08 15:32:31 +0800

    chunk

--- miao/lodash/polikm5-lodash.js
+++ miao/lodash/polikm5-lodash.js
@@ -1,7 +1,21 @@
 var lodash = function() {
-
+  function chunk(arr,size) {
+    let temp = []
+    let res = []
+    for(let i = 0; i < arr.length; i++) {
+      temp.push(arr[i])
+      if(temp.length == size) {
+        res.push(temp)
+        temp = []
+      }
+    }
+    if(temp.length > 0) {
+      res.push(temp)
+    }
+    return res
+  }
 
   return {
-
+    chunk
   }
 }()
